7d5e688348bb801bd4b5c6a74209ec1a3fe8801d
Jooyoung Jang <84753951+0neand0nly@users.noreply.github.com>
2023-05-31 00:00:04 +0900

    update findeq
    still need to work on mutex

--- HW4/findeq.c
+++ HW4/findeq.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdbool.h>
+#include <pthread.h>
 
 #define MAX_THREADS 64
 #define DEFAULT_BYTES 1024
@@ -13,6 +15,8 @@ int numThreads;
 int minSize = DEFAULT_BYTES;
 char *outputPath;
 int file_out = 0;
+
+pthread_mutex_t lock;
 
 typedef struct
 {
@@ -26,6 +30,16 @@ typedef struct
     int start;
     int end;
 } ThreadArgs;
+
+typedef struct node
+{
+    char path[MAX_PATH_LENGTH];
+    struct node * next;
+    
+} duplicateFileList;
+
+
+duplicateFileList *head = NULL;
 
 void getinputs(int argc, char *argv[])
 {
@@ -66,7 +80,64 @@ void getinputs(int argc, char *argv[])
         }
     }
 }
+bool Equalfiles(char *path1, char *path2)
+{
+    FILE *file1 = fopen(path1, "rb");
+    FILE *file2 = fopen(path1, "rb");
 
+    if (file1 == NULL || file2 == NULL)
+    {
+        return false;
+    }
+
+    bool areEqual = true;
+
+    while (true)
+    {
+        int c1 = fgetc(file1);
+        int c2 = fgetc(file2);
+
+        if (c1 != c2)
+        {
+            areEqual = false;
+            break;
+        }
+        if (c1 == EOF || c2 == EOF)
+            break;
+    }
+
+    fclose(file1);
+    fclose(file2);
+    return areEqual;
+}
+void *compareFiles(void *arg)
+{
+    ThreadArgs *args = (ThreadArgs *) arg;
+    FileList *fileList = args -> FileList;
+    int start = args -> start;
+    int end = args -> end;
+
+    for(int i=start;i<end;i++)
+    {
+        for(int j=i+1;j<fileList->count;j++)
+        {
+            if(Equalfiles(fileList->paths[i],fileList->paths[i]))
+            {
+                pthread_mutex_lock(&lock);
+                duplicateFileList * newNode = malloc(sizeof(duplicateFileList));
+                strncpy(newNode->path, fileList->paths[i], MAX_PATH_LENGTH);
+                #ifdef DEBUG
+                printf("File Path added to Dup List: %s\n",fileList->paths[i]);
+                #endif
+                newNode -> next =head;
+                head = newNode;
+                pthread_mutex_unlock(&lock);
+            }
+        }
+    }
+
+    return NULL;
+}
 void traverseDirectory(const char *dir, FileList *filelist)
 {
     struct dirent *entry;
@@ -87,18 +158,28 @@ void traverseDirectory(const char *dir, FileList *filelist)
             continue;
 
         char path[MAX_PATH_LENGTH];
-        snprintf(path, sizeof(path), "%S%S", dir, entry->d_name);
+        snprintf(path, sizeof(path), "%s%s", dir, entry->d_name);
 
-        stat(path, &statbuf);
+        if(stat(path, &statbuf)!=0)
+        {
+            perror("stat");
+            return;
+        }
 
         if (S_ISDIR(statbuf.st_mode))
         {
             traverseDirectory(path, filelist);
         }
-        else
+        else if(statbuf.st_size >= minSize)
         {
+            int debugCount = filelist->count;
+            #ifdef DEBUG
+            printf("File path added: %s\n",path);
+            #endif
             strncpy(filelist->paths[filelist->count++], path, MAX_PATH_LENGTH);
         }
+
+
     }
     closedir(dp);
 }
@@ -110,10 +191,30 @@ int main(int argc, char *argv[])
 
     FileList filelist;
     filelist.count = 0;
+    pthread_mutex_init(&lock,NULL);
 
-    traverseDirectory(dir,&filelist);
 
+    traverseDirectory(dir, &filelist); // traverse files and save the paths to the filelist
+
+    pthread_t threads[numThreads];
+    ThreadArgs threadArgs[numThreads];// init threads and thread args
+
+    int filesPerThread = filelist.count/ numThreads;
     
+    for(int i=0;i<numThreads;i++)
+    {
+        threadArgs[i].FileList = &filelist;
+        threadArgs[i].start = i * filesPerThread;
+        threadArgs[i].end = i+1 * filesPerThread;
+        pthread_create(&threads[i],NULL,compareFiles,&threadArgs[i]);
+    }
+
+    threadArgs[numThreads - 1].end = filelist.count;
+
+    for(int i=0;i<numThreads;i++)
+    {
+        pthread_join(threads[i],NULL);
+    }
 
     return 0;
 }
